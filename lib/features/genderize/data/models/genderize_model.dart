46cc81f2cc3d41c3a710a9e200b4a8fd7e892ce3
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-06-05 23:27:16 +0700

    Reformat kode genderize_model.dart dengan menambahkan koma diakhir dari statement.

--- lib/features/genderize/data/models/genderize_model.dart
+++ lib/features/genderize/data/models/genderize_model.dart
@@ -1,14 +1,22 @@
 import 'package:genderize/features/genderize/domain/entities/genderize.dart';
 
 class GenderizeModel extends Genderize {
-  GenderizeModel({required String name, required String gender})
-      : super(name: name, gender: gender);
+  const GenderizeModel({
+    required String name,
+    required String gender,
+  }) : super(name: name, gender: gender);
 
   factory GenderizeModel.fromJson(Map<String, dynamic> json) {
-    return GenderizeModel(name: json['name'], gender: json['gender']);
+    return GenderizeModel(
+      name: json['name'],
+      gender: json['gender'],
+    );
   }
 
   Map<String, dynamic> toJson() {
-    return {'name': name, 'gender': gender};
+    return {
+      'name': name,
+      'gender': gender,
+    };
   }
 }
