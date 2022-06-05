b4ba0351f9a21f5e9a6e5738df11b521afe14d11
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-06-05 23:26:21 +0700

    Rapikan kode didalam genderize_bloc.dart agar lebih gampang dibaca

--- lib/features/genderize/presentation/bloc/genderize_bloc.dart
+++ lib/features/genderize/presentation/bloc/genderize_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genderize/core/error/failures.dart';
@@ -5,12 +7,12 @@ import 'package:genderize/features/genderize/domain/entities/genderize.dart';
 import 'package:genderize/features/genderize/domain/usecases/get_prediction.dart';
 
 part 'genderize_event.dart';
+
 part 'genderize_state.dart';
 
 const String serverFailureMessage = 'Server Failure';
 const String cacheFailureMessage = 'Cache Failure';
-const String failedPredictionMessage =
-    'Maaf, sistem tidak bisa memprediksi nama ini.';
+const String failedPredictionMessage = 'Maaf, sistem tidak bisa memprediksi nama ini.';
 
 class GenderizeBloc extends Bloc<GenderizeEvent, GenderizeState> {
   final GetPrediction getPrediction;
@@ -18,27 +20,34 @@ class GenderizeBloc extends Bloc<GenderizeEvent, GenderizeState> {
   GenderizeState get initialState => GenderizeInitial();
 
   GenderizeBloc({required this.getPrediction}) : super(GenderizeInitial()) {
-    on<GetPredictionGender>((event, emit) async {
-      emit(GenderizeLoading());
-      var result = await getPrediction(GenderizeParams(name: event.name));
-      result!.fold((failure) {
-        emit(GenderizeError(message: _mapFailureToMessage(failure)));
-      }, (genderize) {
-        emit(GenderizeLoaded(genderize: genderize));
-      });
-    });
+    on<GetPredictionGender>(_onGetPredictionGender);
   }
 
-  String _mapFailureToMessage(Failure failure) {
-    switch (failure.runtimeType) {
-      case ServerFailure:
-        return serverFailureMessage;
-      case CacheFailure:
-        return cacheFailureMessage;
-      case GenderNotFoundFailure:
-        return failedPredictionMessage;
-      default:
-        return 'Unexpected error';
-    }
+  FutureOr<void> _onGetPredictionGender(
+    GetPredictionGender event,
+    Emitter<GenderizeState> emit,
+  ) async {
+    emit(GenderizeLoading());
+    final result = await getPrediction(
+      GenderizeParams(
+        name: event.name,
+      ),
+    );
+    emit(
+      result.fold(
+        (failure) {
+          var errorMessage = 'Unexpected error';
+          if (failure is ServerFailure) {
+            errorMessage = serverFailureMessage;
+          } else if (failure is CacheFailure) {
+            errorMessage = cacheFailureMessage;
+          } else if (failure is GenderNotFoundFailure) {
+            errorMessage = failedPredictionMessage;
+          }
+          return GenderizeError(message: errorMessage);
+        },
+        (genderize) => GenderizeLoaded(genderize: genderize),
+      ),
+    );
   }
 }
