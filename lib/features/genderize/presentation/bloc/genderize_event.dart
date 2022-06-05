772d3cde66e32c0cd73ad74de1568d15fff6ef2b
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-06-05 23:06:06 +0700

    Rapikan kode didalam genderize_event.dart

--- lib/features/genderize/presentation/bloc/genderize_event.dart
+++ lib/features/genderize/presentation/bloc/genderize_event.dart
@@ -2,13 +2,20 @@ part of 'genderize_bloc.dart';
 
 abstract class GenderizeEvent extends Equatable {
   const GenderizeEvent();
-
-  @override
-  List<Object> get props => [];
 }
 
 class GetPredictionGender extends GenderizeEvent {
   final String name;
 
-  GetPredictionGender(this.name);
+  const GetPredictionGender(this.name);
+
+  @override
+  List<Object?> get props => [
+    name,
+  ];
+
+  @override
+  String toString() {
+    return 'GetPredictionGender{name: $name}';
+  }
 }
