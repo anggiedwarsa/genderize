0d981588e963d900221d01613ed0c6347aacd5ee
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-06-05 22:51:07 +0700

    Rapikan kode didalam genderize_local_data_source.dart
    1. Ubah tipe data return value dari function `cacheGender` menjadi `bool`.
    2. Perbaiki penggunaan null safety dari function `getPrediction`.
    3. Rapikan kode didalam function implement `getPrediction` agar lebih gampang dibaca.

--- lib/features/genderize/data/datasources/genderize_local_data_source.dart
+++ lib/features/genderize/data/datasources/genderize_local_data_source.dart
@@ -5,8 +5,9 @@ import 'package:genderize/features/genderize/data/models/genderize_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class GenderizeLocalDataSource {
-  Future<GenderizeModel>? getPrediction();
-  Future<void>? cacheGender(GenderizeModel genderToCache);
+  Future<GenderizeModel?> getPrediction();
+
+  Future<bool> cacheGender(GenderizeModel genderToCache);
 }
 
 const cacheGenderize = 'CACHE_GENDERIZE';
@@ -17,17 +18,19 @@ class GenderizeLocalDataSourceImpl implements GenderizeLocalDataSource {
   GenderizeLocalDataSourceImpl({required this.sharedPreferences});
 
   @override
-  Future<GenderizeModel>? getPrediction() {
+  Future<GenderizeModel?> getPrediction() async {
     final jsonString = sharedPreferences.getString(cacheGenderize);
     if (jsonString != null) {
-      return Future.value(GenderizeModel.fromJson(json.decode(jsonString)));
+      final jsonData = json.decode(jsonString);
+      final data = GenderizeModel.fromJson(jsonData);
+      return data;
     } else {
       throw CacheException();
     }
   }
 
   @override
-  Future<void>? cacheGender(GenderizeModel genderToCache) {
+  Future<bool> cacheGender(GenderizeModel genderToCache) {
     return sharedPreferences.setString(
         cacheGenderize, json.encode(genderToCache.toJson()));
   }
