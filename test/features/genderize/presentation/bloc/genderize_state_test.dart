46815962c4681a13a3d583c3423413bd5a5d4c6b
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-06-06 22:23:24 +0700

    Buat unit test untuk file genderize_state.dart

---
+++ test/features/genderize/presentation/bloc/genderize_state_test.dart
@@ -0,0 +1,119 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:genderize/features/genderize/domain/entities/genderize.dart';
+import 'package:genderize/features/genderize/presentation/bloc/genderize_bloc.dart';
+
+void main() {
+  group('GenderizeInitial', () {
+    final tState = GenderizeInitial();
+
+    test(
+      'pastikan output dari nilai props',
+      () async {
+        // assert
+        expect(
+          tState.props,
+          [],
+        );
+      },
+    );
+
+    test(
+      'pastikan output dari fungsi toString',
+      () async {
+        // assert
+        expect(
+          tState.toString(),
+          'GenderizeInitial()',
+        );
+      },
+    );
+  });
+
+  group('GenderizeLoading', () {
+    final tState = GenderizeLoading();
+
+    test(
+      'pastikan output dari nilai props',
+      () async {
+        // assert
+        expect(
+          tState.props,
+          [],
+        );
+      },
+    );
+
+    test(
+      'pastikan output dari fungsi toString',
+      () async {
+        // assert
+        expect(
+          tState.toString(),
+          'GenderizeLoading()',
+        );
+      },
+    );
+  });
+
+  group('GenderizeLoaded', () {
+    const tGenderize = Genderize(
+      name: 'testName',
+      gender: 'testGender',
+    );
+    const tState = GenderizeLoaded(
+      genderize: tGenderize,
+    );
+
+    test(
+      'pastikan output dari nilai props',
+      () async {
+        // assert
+        expect(
+          tState.props,
+          [
+            tState.genderize,
+          ],
+        );
+      },
+    );
+
+    test(
+      'pastikan output dari fungsi toString',
+      () async {
+        // assert
+        expect(
+          tState.toString(),
+          'GenderizeLoaded{genderize: ${tState.genderize}}',
+        );
+      },
+    );
+  });
+
+  group('GenderizeError', () {
+    const tState = GenderizeError(message: 'testMessage');
+
+    test(
+      'pastikan output dari nilai props',
+      () async {
+        // assert
+        expect(
+          tState.props,
+          [
+            tState.message,
+          ],
+        );
+      },
+    );
+
+    test(
+      'pastikan output dari fungsi toString',
+      () async {
+        // assert
+        expect(
+          tState.toString(),
+          'GenderizeError{message: ${tState.message}}',
+        );
+      },
+    );
+  });
+}
