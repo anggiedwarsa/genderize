e90e9af12caeee089b1773d96b8b580fe8843f8b
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-06-06 17:55:14 +0700

    Buat unit test dari file genderize.dart yang didalam directory entities

---
+++ test/features/genderize/domain/entities/genderize_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:genderize/features/genderize/domain/entities/genderize.dart';
+
+void main() {
+  const tEntity = Genderize(
+    name: 'testName',
+    gender: 'testGender',
+  );
+
+  test(
+    'pastikan output dari nilai props',
+    () async {
+      // assert
+      expect(
+        tEntity.props,
+        [
+          tEntity.name,
+          tEntity.gender,
+        ],
+      );
+    },
+  );
+
+  test(
+    'pastikan output dari fungsi toString',
+    () async {
+      // assert
+      expect(
+        tEntity.toString(),
+        'Genderize{name: ${tEntity.name}, gender: ${tEntity.gender}}',
+      );
+    },
+  );
+}
