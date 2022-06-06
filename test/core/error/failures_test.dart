58e21aaa4efea56db585b7cbdd6e99beae8699b8
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-06-06 17:48:35 +0700

    Buat unit test dari file failures.dart

---
+++ test/core/error/failures_test.dart
@@ -0,0 +1,110 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:genderize/core/error/failures.dart';
+
+void main() {
+  group('ServerFailure', () {
+    final tFailure = ServerFailure();
+
+    test(
+      'pastikan output dari nilai props',
+      () async {
+        // assert
+        expect(
+          tFailure.props,
+          [],
+        );
+      },
+    );
+
+    test(
+      'pastikan output dari fungsi toString',
+      () async {
+        // assert
+        expect(
+          tFailure.toString(),
+          'ServerFailure()',
+        );
+      },
+    );
+  });
+
+  group('CacheFailure', () {
+    final tFailure = CacheFailure();
+
+    test(
+      'pastikan output dari nilai props',
+      () async {
+        // assert
+        expect(
+          tFailure.props,
+          [],
+        );
+      },
+    );
+
+    test(
+      'pastikan output dari fungsi toString',
+      () async {
+        // assert
+        expect(
+          tFailure.toString(),
+          'CacheFailure()',
+        );
+      },
+    );
+  });
+
+  group('ConnectionFailure', () {
+    final tFailure = ConnectionFailure();
+
+    test(
+      'pastikan output dari nilai props',
+      () async {
+        // assert
+        expect(
+          tFailure.props,
+          [
+            tFailure.errorMessage,
+          ],
+        );
+      },
+    );
+
+    test(
+      'pastikan output dari fungsi toString',
+      () async {
+        // assert
+        expect(
+          tFailure.toString(),
+          'ConnectionFailure{errorMessage: ${tFailure.errorMessage}}',
+        );
+      },
+    );
+  });
+
+  group('GenderNotFoundFailure', () {
+    final tFailure = GenderNotFoundFailure();
+
+    test(
+      'pastikan output dari nilai props',
+      () async {
+        // assert
+        expect(
+          tFailure.props,
+          [],
+        );
+      },
+    );
+
+    test(
+      'pastikan output dari fungsi toString',
+      () async {
+        // assert
+        expect(
+          tFailure.toString(),
+          'GenderNotFoundFailure()',
+        );
+      },
+    );
+  });
+}
