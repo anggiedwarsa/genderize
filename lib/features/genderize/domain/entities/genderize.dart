16cecd1c7b25366fc233f9b076ab6cc4853fc0aa
ismailhadiputra <ismailhadiputra@nusa.net.id>
2022-06-07 15:17:11 +0700

    Implementasi JsonSerializable di model

--- lib/features/genderize/domain/entities/genderize.dart
+++ lib/features/genderize/domain/entities/genderize.dart
@@ -1,7 +1,11 @@
 import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+@JsonSerializable()
 class Genderize extends Equatable {
+  @JsonKey(name: 'name')
   final String? name;
+  @JsonKey(name: 'gender')
   final String? gender;
 
   const Genderize({required this.name, required this.gender});
