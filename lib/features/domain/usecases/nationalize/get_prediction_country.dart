9c31c10a33746982d8cbed1d7a126ddd7c566b92
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-06-10 08:51:34 +0700

    Update use case get_prediction_country.dart agar menggunakan class model NationalizeModel

--- lib/features/domain/usecases/nationalize/get_prediction_country.dart
+++ lib/features/domain/usecases/nationalize/get_prediction_country.dart
@@ -2,17 +2,17 @@ import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:genderize/core/error/failures.dart';
 import 'package:genderize/core/usecases/usecases.dart';
-import 'package:genderize/features/domain/entities/nationalize/nationalize.dart';
+import 'package:genderize/features/data/models/nationalize/nationalize_model.dart';
 import 'package:genderize/features/domain/repositories/nationalize/nationalize_repository.dart';
 
-class GetPredictionCountry implements UseCase<Nationalize, NationalizeParams> {
+class GetPredictionCountry implements UseCase<NationalizeModel, NationalizeParams> {
   final NationalizeRepository nationalizeRepository;
 
   GetPredictionCountry(this.nationalizeRepository);
 
   @override
-  Future<Either<Failure, Nationalize>> call(NationalizeParams params) async {
-    return await nationalizeRepository.getPredictionCountry(params.name);
+  Future<Either<Failure, NationalizeModel>> call(NationalizeParams params) async {
+    return nationalizeRepository.getPredictionCountry(params.name);
   }
 }
 
